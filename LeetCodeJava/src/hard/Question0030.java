5a2c9a566b97b29651240127a468935722493c21
Ke Yang <734743339@qq.com>
2021-10-11 14:59:20 -0400

    Question0030(1.0)

--- LeetCodeJava/src/hard/Question0030.java
+++ LeetCodeJava/src/hard/Question0030.java
@@ -6,13 +6,15 @@ package hard;
  * @date 9/23/21
  */
 
+import com.sun.nio.sctp.SendFailedNotification;
+
 import java.util.*;
 
 /**
  * You are given a string s and an array of strings words of the same length.
  * Return all starting indices of substring(s) in s that is a concatenation of each word in words exactly once,
  * in any order, and without any intervening characters.
- *
+ * <p>
  * You can return the answer in any order.
  */
 public class Question0030 {
@@ -31,12 +33,12 @@ public class Question0030 {
             for (int j = 0; j < L - l + 1; j++) {
                 boolean isOk = true;
                 for (int i = 0; i < l; i++) {
-                    if (chars[j+i] != wordChars[i]) {
-                        isOk =false;
+                    if (chars[j + i] != wordChars[i]) {
+                        isOk = false;
                         break;
                     }
                 }
-                if (isOk){
+                if (isOk) {
                     list.add(j);
                 }
             }
@@ -58,15 +60,15 @@ public class Question0030 {
         for (Map.Entry<Integer, Map<String, List<Integer>>> entry : tmpMap.entrySet()) {
 
             List<Integer> allIndices = new ArrayList<>();
-            for (Map.Entry<String, List<Integer>> entry1 : entry.getValue().entrySet()){
+            for (Map.Entry<String, List<Integer>> entry1 : entry.getValue().entrySet()) {
                 allIndices.addAll(entry1.getValue());
             }
             Collections.sort(allIndices);
 
-            for (int i = 0 ; i <= allIndices.size() - words.length; i++){
+            for (int i = 0; i <= allIndices.size() - words.length; i++) {
                 boolean isOk = true;
-                for (int j = 0; j < words.length - 1; j++){
-                    if(allIndices.get(i)+l != allIndices.get(i+1)) {
+                for (int j = 0; j < words.length - 1; j++) {
+                    if (allIndices.get(i) + l != allIndices.get(i + 1)) {
                         isOk = false;
                         break;
                     }
@@ -78,18 +80,18 @@ public class Question0030 {
         }
 
         Arrays.sort(words);
-        for (int index : tmpResult){
+        for (int index : tmpResult) {
             String[] tmpWords = new String[words.length];
-            for (int i = 0 ; i < words.length; i++){
+            for (int i = 0; i < words.length; i++) {
                 String str = "";
                 for (int j = 0; j < l; j++) {
-                    str += chars[index+i*l + j];
+                    str += chars[index + i * l + j];
                 }
                 tmpWords[i] = str;
             }
             Arrays.sort(tmpWords);
 
-            if (Arrays.equals(words, tmpWords)){
+            if (Arrays.equals(words, tmpWords)) {
                 result.add(index);
             }
         }
@@ -97,10 +99,50 @@ public class Question0030 {
         return result;
     }
 
+    /**
+     * Copy from Jianchao Li
+     * @param s
+     * @param words
+     * @return
+     */
+    public List<Integer> findSubstring3(String s, String[] words) {
+        List<Integer> results = new ArrayList<Integer>();
+        Map<String, Integer> counts = new HashMap<String, Integer>();
+        for (String word : words) {
+            counts.put(word, counts.getOrDefault(word, 0) + 1);
+        }
+
+        int stringLength = s.length();
+        int wordLength = words[0].length();
+        int num = words.length;
+
+        for (int i = 0; i < stringLength - wordLength * num + 1; i++) {
+            Map<String, Integer> seen = new HashMap<String, Integer>();
+            int j = 0;
+            for (; j < num; j++) {
+              String tmp = s.substring(i+j*wordLength, i+(j+1)*wordLength );
+              if (counts.containsKey(tmp)) {
+                  seen.put(tmp, seen.getOrDefault(tmp, 0) + 1);
+                  if (seen.getOrDefault(tmp, 0) > counts.getOrDefault(tmp, 0)) { // the most important part of this method.
+                      break;
+                  }
+              }else {
+                  break;
+              }
+            }
+            if (j == num) {
+                results.add(i);
+            }
+
+        }
+        return results;
+    }
+
     public static void main(String[] args) {
         String s = "ababaab";
-        String[] words = {"ab","ba","ba"};
+        String[] words = {"ab", "ba", "ba"};
         Question0030 q = new Question0030();
         System.out.println(q.findSubstring(s, words));
+        System.out.println(q.findSubstring3(s, words));
     }
 }

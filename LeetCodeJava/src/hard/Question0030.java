0c235abf8250ef1042867d6e3073362dc7622619
Ke Yang <734743339@qq.com>
2021-10-13 18:43:23 -0400

    Question0032(1.0)

--- LeetCodeJava/src/hard/Question0030.java
+++ LeetCodeJava/src/hard/Question0030.java
@@ -11,6 +11,7 @@ import com.sun.nio.sctp.SendFailedNotification;
 import java.util.*;
 
 /**
+ * QUESTION 30
  * You are given a string s and an array of strings words of the same length.
  * Return all starting indices of substring(s) in s that is a concatenation of each word in words exactly once,
  * in any order, and without any intervening characters.

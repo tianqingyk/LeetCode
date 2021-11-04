0352d8e862ae86570afc9a08b30562e3e1ed8d14
Ke Yang <734743339@qq.com>
2021-11-04 19:16:38 -0400

    Question 41(1.0)

---
+++ LeetCodeJava/src/hard/Question0041.java
@@ -0,0 +1,62 @@
+package hard;
+
+/**
+ * @author yangke
+ * @projectName LeetCodeJava
+ * @date 11/4/21
+ */
+
+/**
+ * QUESTION 41
+ * Given an unsorted integer array nums, return the smallest missing positive integer.
+ * <p>
+ * You must implement an algorithm that runs in O(n) time and uses constant extra space.
+ */
+public class Question0041 {
+
+    public int firstMissingPositive(int[] nums) {
+        int l = nums.length;
+        for (int i = 0; i < l; i++) {
+            int tmp = nums[i];
+            if (tmp == i + 1) {
+                continue;
+            }
+
+            nums[i] = 0;
+            if (tmp < 1 || tmp > l) {
+                nums[i] = 0;
+                continue;
+            }
+            // 1 <= tmp <=l
+            if (tmp < i + 1) {
+                nums[tmp - 1] = tmp;
+                continue;
+            }
+
+            putNextNum(tmp, nums);
+        }
+
+        for (int i = 0; i < l; i++) {
+            if (nums[i] == 0) {
+                return i + 1;
+            }
+        }
+        return l+1;
+    }
+
+    private void putNextNum(int tmp, int[] nums) {
+        int l = nums.length;
+        int next = nums[tmp - 1];
+        nums[tmp - 1] = tmp;
+        if (next < 1 || next > l || next == tmp) {
+            return;
+        }
+        putNextNum(next, nums);
+    }
+
+    public static void main(String[] args){
+        Question0041 q = new Question0041();
+        int[] nums = {2,2};
+        System.out.println(q.firstMissingPositive(nums));;
+    }
+}

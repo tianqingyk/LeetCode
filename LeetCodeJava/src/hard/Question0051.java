6db18c95e117955f94ea3a3f5f266d8b7058a662
Ke Yang <734743339@qq.com>
2021-11-25 13:05:21 -0500

    Question0051(2.0)

--- LeetCodeJava/src/hard/Question0051.java
+++ LeetCodeJava/src/hard/Question0051.java
@@ -12,54 +12,101 @@ public class Question0051 {
     /**
      * QUESTION 51 N-Queens
      * The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
-     *
+     * <p>
      * Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
-     *
+     * <p>
      * Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
      */
 
     public List<List<String>> solveNQueens(int n) {
-        boolean[][] matrix = new boolean[n][n];
+        List<List<Integer>> result = new ArrayList<>();
+        List<List<String>> resultStr = new ArrayList<>();
 
-        int[] tmpIndex = new int[n];
-        for ( int i = 0; i < n; i++) {
-           tmpIndex[i] = -1;
-        }
+        List<List<Integer>> nPermutation = getNPermutation(n);
+        for (List<Integer> list : nPermutation) {
+            boolean[][] matrix = new boolean[n][n];
 
-        for (int i = 0; i < n; i++){
-            List<Integer> posList = getPossiblePos(i, matrix);
-            boolean isOk = false;
-            for (int pos : posList){
-                if (pos > tmpIndex[i]){
-                    tmpIndex[i] = pos;
-                    putQueen(i, pos, matrix);
-                    isOk = true;
+            boolean isOk = true;
+            for (int i = 0; i < n; i++){
+                if (!putQueen(i, list.get(i), matrix, n)){
+                    isOk = false;
                     break;
                 }
             }
-            if (!isOk){
-                
+            if (isOk){
+                result.add(list);
             }
         }
-        return null;
+
+        for (List<Integer> list : result){
+            List<String> tmp = new ArrayList<>();
+            for (Integer i : list){
+                tmp.add(produceStr(i, n));
+            }
+            resultStr.add(tmp);
+        }
+        return resultStr;
     }
 
-    private List<Integer> getPossiblePos(int i, boolean[][] matrix){
-        List<Integer> result = new ArrayList<>();
-        for (int j = 0; j < matrix[i].length; j++){
-            if (!matrix[i][j]){
-                result.add(j);
+    private String produceStr(int i, int n){
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < n; j++){
+            if (j == i){
+                sb.append('Q');
+            }else {
+                sb.append('.');
+            }
+        }
+        return sb.toString();
+    }
+
+    private boolean putQueen(int i, int j, boolean[][] matrix, int n) {
+        if (matrix[i][j]){
+            return false;
+        }
+        for (int k = 0; k < n; k ++){
+            matrix[i][k] = true;
+            matrix[k][j] = true;
+            if (i+k < n){
+                if (j+k < n){
+                    matrix[i+k][j+k] = true;
+                }
+                if (j-k >= 0) {
+                    matrix[i + k][j - k] = true;
+                }
+            }
+        }
+        return true;
+    }
+
+    private List<List<Integer>> getNPermutation(int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (n <= 1) {
+            List<Integer> tmp = new ArrayList<>();
+            tmp.add(0);
+            result.add(tmp);
+            return result;
+        }
+        for (int i = 0; i < n; i++) {
+            for (List<Integer> list : getNPermutation(n - 1)) {
+                List<Integer> tmp = new ArrayList<>();
+                for (int j = 0; j < n; j++) {
+                    if (j == i) {
+                        tmp.add(0);
+                    } else if (j < i) {
+                        tmp.add(list.get(j) + 1);
+                    } else {
+                        tmp.add(list.get(j - 1) + 1);
+                    }
+                }
+                result.add(tmp);
             }
         }
         return result;
     }
 
-    private void putQueen(int i, int pos, boolean[][] matrix){
-
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Question0051 q = new Question0051();
-        q.solveNQueens(4);
+        System.out.println(q.solveNQueens(3));
     }
 }

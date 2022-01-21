cc31075d5003d678181238e3ba760a29faa66606
Ke Yang <734743339@qq.com>
2022-01-20 20:10:06 -0500

    Question0164(2.0) bucket the pigeonhole principle

--- LeetCodeJava/src/hard/Question0164.java
+++ LeetCodeJava/src/hard/Question0164.java
@@ -1,8 +1,6 @@
 package hard;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author yangk
@@ -13,23 +11,31 @@ public class Question0164 {
     /**
      * 164. Maximum Gap
      * Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
-     *
+     * <p>
      * You must write an algorithm that runs in linear time and uses linear extra space.
      */
 
     public int maximumGap(int[] nums) {
         List<Integer> list = new ArrayList<>();
-        for (int num  : nums) {
+        for (int num : nums) {
             list.add(num);
         }
         return maximumGap(list);
     }
 
-    public int maximumGap(List<Integer> nums){
-        if (nums.size() <= 1){
+    /**
+     * Solution 1
+     * Runtime: 219 ms, faster than 7.56% of Java online submissions for Maximum Gap.
+     * Memory Usage: 50.6 MB, less than 81.33% of Java online submissions for Maximum Gap.
+     *
+     * @param nums
+     * @return
+     */
+    public int maximumGap(List<Integer> nums) {
+        if (nums.size() <= 1) {
             return 0;
         }
-        if (nums.size() <= 2){
+        if (nums.size() <= 2) {
             return Math.abs(nums.get(1) - nums.get(0));
         }
 
@@ -41,18 +47,18 @@ public class Question0164 {
 
         for (int i = 1; i < nums.size(); i++) {
             int num = nums.get(i);
-            if (num <= cp){
+            if (num <= cp) {
                 leftList.add(num);
                 leftMin = Math.min(leftMin, cp - num);
-            }else{
+            } else {
                 rightList.add(num);
                 rightMin = Math.min(rightMin, num - cp);
             }
         }
-        if (leftMin >= Integer.MAX_VALUE){
+        if (leftMin >= Integer.MAX_VALUE) {
             leftMin = 0;
         }
-        if (rightMin >= Integer.MAX_VALUE){
+        if (rightMin >= Integer.MAX_VALUE) {
             rightMin = 0;
         }
         int max = Math.max(leftMin, rightMin);
@@ -62,8 +68,101 @@ public class Question0164 {
 
     }
 
+    /**
+     * Solution 2 copy from discuss
+     *
+     * @param nums
+     * @return
+     */
+    public int maximumGap2(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        // m is the maximal number in nums
+        int m = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            m = Math.max(m, nums[i]);
+        }
+
+        int exp = 1; // 1, 10, 100, 1000 ...
+        int R = 10; // 10 digits
+
+        int[] aux = new int[nums.length];
+
+        while (m / exp > 0) { // Go through all digits from LSB to MSB
+            int[] count = new int[R];
+
+            for (int i = 0; i < nums.length; i++) {
+                count[(nums[i] / exp) % 10]++;
+            }
+
+            for (int i = 1; i < count.length; i++) {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = nums.length - 1; i >= 0; i--) {
+                aux[--count[(nums[i] / exp) % 10]] = nums[i];
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = aux[i];
+            }
+            exp *= 10;
+        }
+
+        int max = 0;
+        for (int i = 1; i < aux.length; i++) {
+            max = Math.max(max, aux[i] - aux[i - 1]);
+        }
+
+        return max;
+    }
+
+    /**
+     * solution 3 buckets and The Pigeonhole Principle
+     * Runtime: 527 ms, faster than 5.04% of Java online submissions for Maximum Gap.
+     * Memory Usage: 222.7 MB, less than 5.04% of Java online submissions for Maximum Gap.
+     * @param nums
+     * @return
+     */
+    public int maximumGap3(int[] nums) {
+        int n = nums.length;
+        if(n < 2){
+            return 0;
+        }
+        int max = Arrays.stream(nums).max().getAsInt();
+        int min = Arrays.stream(nums).min().getAsInt();
+        int gap = Math.max((max - min) / (n - 1), 1);
+
+        Map<Integer, List<Integer>> buckets = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int key = (nums[i] - min) / gap;
+            buckets.compute(key, (k, v) -> v == null ? new ArrayList<>() : v).add(nums[i]);
+        }
+
+        List<Integer> tmpList = new ArrayList<>();
+        for (int i = n; i >= 0; i--) {
+            if (buckets.containsKey(i)) {
+                if (tmpList.size() > 0) {
+                    tmpList.add(buckets.get(i).stream().max(Integer::compare).get());
+                }
+                if (i > 0) {
+                    tmpList.add(buckets.get(i).stream().min(Integer::compare).get());
+                }
+            }
+        }
+
+        max = 0;
+        for (int i = 0; i < tmpList.size() - 1; i++) {
+            max = Math.max(max, tmpList.get(i) - tmpList.get(i + 1));
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         Question0164 q = new Question0164();
-        q.maximumGap(new int[] {9,19});
+        int reulst = q.maximumGap3(new int[]{1,1,1,1});
+        System.out.println(reulst);
     }
 }

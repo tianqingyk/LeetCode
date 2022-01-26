ff3a034409d543947bda14bafcab39d99c1d387f
Ke Yang <734743339@qq.com>
2022-01-26 18:03:22 -0500

    Question0174(1.0)

--- LeetCodeJava/src/hard/Question0185.java
+++
@@ -1,9 +0,0 @@
-package hard;
-
-/**
- * @author yangk
- * @projectName LeetCode
- * @data 2021/12/27
- */
-public class Question0185 {
-}
--- LeetCodeJava/src/hard/Question0188.java
+++ LeetCodeJava/src/hard/Question0188.java
@@ -6,4 +6,47 @@ package hard;
  * @data 2021/12/27
  */
 public class Question0188 {
+
+    /**
+     * 188. Best Time to Buy and Sell Stock IV
+     *
+     * You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
+     *
+     * Find the maximum profit you can achieve. You may complete at most k transactions.
+     *
+     * Note: You may not engage in multiple transactions simultaneously(i.e., you must sell the stock before you buy again).
+     */
+
+    /**
+     * Solution 1
+     * Runtime: 1 ms, faster than 99.82% of Java online submissions for Best Time to Buy and Sell Stock IV.
+     * Memory Usage: 36.7 MB, less than 92.73% of Java online submissions for Best Time to Buy and Sell Stock IV.
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int k, int[] prices){
+        if (k < 1){
+            return 0;
+        }
+        int[] dp = new int[2*k];
+        for (int i = 0; i < k; i++) {
+            dp[i] = Integer.MIN_VALUE;
+            dp[i+1] = Integer.MIN_VALUE;
+        }
+
+        for (int i = 0; i < prices.length; i++) {
+            int price = prices[i];
+            for (int j = 0; j < k; j++) {
+                dp[2*j] = j > 0 ? Math.max(dp[2*j], dp[2*j-1] -price) : Math.max(dp[2*j], -price);
+                dp[2*j+1] = Math.max(price + dp[2*j], dp[2*j+1]);
+            }
+        }
+        return dp[2*k-1];
+    }
+
+    public static void main(String[] args) {
+        Question0188 q = new Question0188();
+        q.maxProfit(2, new int[]{2,4,1});
+    }
 }

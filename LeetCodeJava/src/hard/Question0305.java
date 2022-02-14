c6246d56bc7ec719216e63522db30466c14a9da4
Ke Yang <734743339@qq.com>
2022-02-14 12:58:35 -0500

    Question0305(2.0) solutions copy from solution

--- LeetCodeJava/src/hard/Question0305.java
+++ LeetCodeJava/src/hard/Question0305.java
@@ -47,7 +47,7 @@ public class Question0305 {
                 continue;
             }
 
-            if (cache.containsKey(positionStr)){
+            if (cache.containsKey(positionStr)) {
                 result.add(result.get(i - 1));
                 continue;
             }
@@ -112,4 +112,158 @@ public class Question0305 {
     private String getPositionStr(int[] position) {
         return getPositionStr(position[0], position[1]);
     }
+
+    /**
+     * Solution 2 copy from solution
+     * (Ad hoc) 临时
+     * The only difference between solution 2 and solution 1 is to set an id for each island
+     * Runtime: 1239 ms, faster than 5.03% of Java online submissions for Number of Islands II.
+     * Memory Usage: 49 MB, less than 63.81% of Java online submissions for Number of Islands II.
+     */
+
+    public List<Integer> numIslands22(int m, int n, int[][] positions) {
+        List<Integer> ans = new ArrayList<>();
+        HashMap<Integer, Integer> land2id = new HashMap<Integer, Integer>();
+        int num_islands = 0;
+        int island_id = 0; // Set id for each island
+
+        for (int[] pos : positions) {
+
+            int r = pos[0], c = pos[1];
+            if (land2id.containsKey(r * n + c)) {
+                ans.add(num_islands);
+                continue;
+            }
+            Set<Integer> overlap = new HashSet<Integer>();
+
+            if (r - 1 >= 0 && land2id.containsKey((r - 1) * n + c)) {
+                overlap.add(land2id.get((r - 1) * n + c));
+            }
+            if (r + 1 < m && land2id.containsKey((r + 1) * n + c)) {
+                overlap.add(land2id.get((r + 1) * n + c));
+            }
+            if (c - 1 >= 0 && land2id.containsKey(r * n + c - 1)) {
+                overlap.add(land2id.get(r * n + c - 1));
+            }
+            if (c + 1 < n && land2id.containsKey(r * n + c + 1)) {
+                overlap.add(land2id.get(r * n + c + 1));
+            }
+
+            if (overlap.isEmpty()) {
+                ++num_islands;
+                land2id.put(r * n + c, island_id++);
+            } else if (overlap.size() == 1) {
+                land2id.put(r * n + c, overlap.iterator().next());
+            } else {
+                int root_id = overlap.iterator().next();
+                for (Map.Entry<Integer, Integer> entry : land2id.entrySet()) {
+                    int k = entry.getKey();
+                    int id = entry.getValue();
+                    if (overlap.contains(id)) {
+                        land2id.put(k, root_id);
+                    }
+                }
+                land2id.put(r * n + c, root_id);
+                num_islands -= (overlap.size() - 1);
+            }
+            ans.add(num_islands);
+        }
+
+        return ans;
+    }
+
+    /**
+     * Solution 3 copy from solution
+     * Union Find(aka Disjoint Set)
+     */
+
+    class UnionFind {
+        int count; // # of connected components
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) { // for problem 200
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public UnionFind(int N) { // for problem 305 and others
+            count = 0;
+            parent = new int[N];
+            rank = new int[N]; // make the structure of the tree squat
+            for (int i = 0; i < N; ++i) {
+                parent[i] = -1;
+                rank[i] = 0;
+            }
+        }
+
+        public boolean isValid(int i) { // for problem 305
+            return parent[i] >= 0;
+        }
+
+        public void setParent(int i) {
+            parent[i] = i;
+            ++count;
+        }
+
+        public int find(int i) { // path compression
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) { // union with rank
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx;
+                    rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public List<Integer> numIslands23(int m, int n, int[][] positions) {
+        List<Integer> ans = new ArrayList<>();
+        UnionFind uf = new UnionFind(m * n);
+
+        for (int[] pos : positions) {
+            int r = pos[0], c = pos[1];
+            List<Integer> overlap = new ArrayList<>();
+
+            if (r - 1 >= 0 && uf.isValid((r - 1) * n + c)) overlap.add((r - 1) * n + c);
+            if (r + 1 < m && uf.isValid((r + 1) * n + c)) overlap.add((r + 1) * n + c);
+            if (c - 1 >= 0 && uf.isValid(r * n + c - 1)) overlap.add(r * n + c - 1);
+            if (c + 1 < n && uf.isValid(r * n + c + 1)) overlap.add(r * n + c + 1);
+
+            int index = r * n + c;
+            uf.setParent(index);
+            for (int i : overlap) uf.union(i, index);
+            ans.add(uf.getCount());
+        }
+
+        return ans;
+    }
+
 }

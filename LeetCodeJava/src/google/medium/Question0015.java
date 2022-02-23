db604450ab554e655b87c51d71fc7579d87657fa
tianqingyk <734743339@qq.com>
2022-02-23 16:28:50 -0500

    15. 3Sum(2.0) Copy From Solution

--- LeetCodeJava/src/google/medium/Question0015.java
+++ LeetCodeJava/src/google/medium/Question0015.java
@@ -7,7 +7,7 @@ import java.util.*;
  * @projectName LeetCode
  * @data 2022/2/23
  */
-public class Question00015 {
+public class Question0015 {
 
     /**
      * 15. 3Sum
@@ -66,5 +66,64 @@ public class Question00015 {
             }
         }
         return result.stream().toList();
+    }
+
+    /**
+     * Solution 2 Copy from Solution
+     * Two Pointers
+     */
+
+    public List<List<Integer>> threeSum2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if (i == 0 || nums[i-1] != nums[i]) {
+                twoSumII(nums, i, res);
+            }
+        }
+        return res;
+    }
+
+    private void twoSumII(int[] nums, int i, List<List<Integer>> res) {
+        int lo = i + 1, hi = nums.length - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
+                while (lo < hi && nums[lo] == nums[lo - 1])
+                    ++lo;
+            }
+        }
+    }
+
+    /**
+     * Solution 3
+     * Hashset
+     */
+
+    public List<List<Integer>> threeSum3(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSum(nums, i, res);
+            }
+        return res;
+    }
+    void twoSum(int[] nums, int i, List<List<Integer>> res) {
+        var seen = new HashSet<Integer>();
+        for (int j = i + 1; j < nums.length; ++j) {
+            int complement = -nums[i] - nums[j];
+            if (seen.contains(complement)) {
+                res.add(Arrays.asList(nums[i], nums[j], complement));
+                while (j + 1 < nums.length && nums[j] == nums[j + 1])
+                    ++j;
+            }
+            seen.add(nums[j]);
+        }
     }
 }
